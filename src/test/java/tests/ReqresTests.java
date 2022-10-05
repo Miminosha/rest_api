fe60980b6335298f2add172f3a2815cfaf8994da
Miminosha <anna.memikova@yandex.ru>
2022-10-05 23:17:27 +0300

    wrote reqres rest-assured tests

---
+++ src/test/java/tests/ReqresTests.java
@@ -0,0 +1,91 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.is;
+
+public class ReqresTests {
+
+    @Test
+    void registerSuccessfulTest(){
+        String body = "{ \"email\": \"eve.holt@reqres.in\", \"password\": \"pistol\" }";
+
+        given()
+                .log().uri()
+                .log().body()
+                .contentType(JSON)
+                .body(body)
+                .when()
+                .post("https://reqres.in/api/register")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("id", is(4));
+    }
+
+    @Test
+    void registerUnsuccessfulTest(){
+        String body = "{ \"email\": \"eve.holt@reqres.in\" }";
+
+        given()
+                .log().uri()
+                .log().body()
+                .contentType(JSON)
+                .body(body)
+                .when()
+                .post("https://reqres.in/api/register")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+    }
+
+    @Test
+    void createTest(){
+        String body = "{ \"name\": \"morpheus\", \"job\": \"leader\" }";
+
+        given()
+                .log().uri()
+                .log().body()
+                .contentType(JSON)
+                .body(body)
+                .when()
+                .post("https://reqres.in/api/users")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(201);
+    }
+    @Test
+    void createResponseHasSameNameTest(){
+        String body = "{ \"name\": \"morpheus\", \"job\": \"leader\" }";
+
+        given()
+                .log().uri()
+                .log().body()
+                .contentType(JSON)
+                .body(body)
+                .when()
+                .post("https://reqres.in/api/users")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(201)
+                .body("name", is("morpheus"));
+    }
+
+    @Test
+    void singleUserGetTest(){
+        given()
+                .log().all()
+                .when()
+                .get("https://reqres.in/api/users/2")
+                .then()
+                .statusCode(200)
+                .body("data.id", is(2));
+    }
+}
---
+++ src/test/java/tests/SelenoidTests.java
@@ -0,0 +1,152 @@
+package tests;
+
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SelenoidTests {
+
+        /*
+    1. Make POST request to https://reqres.in/api/login with body { "email": "eve.holt@reqres.in", "password": "cityslicka" }
+    2. Get response { "token": "QpwL5tke4Pnpja7X4" }
+    3. Check token is "QpwL5tke4Pnpja7X4"
+     */
+
+    @Test
+    void checkTotal() {
+        get("https://selenoid.autotests.cloud/status")
+                .then()
+                .statusCode(200)
+                .body("total", is(20));
+    }
+
+    @Test
+    void checkTotalWithGiven() {
+        given()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .statusCode(200)
+                .body("total", is(20));
+    }
+
+    @Test
+    void checkTotalWithLogs() {
+        given()
+                .log().all()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("total", is(20));
+    }
+
+    @Test
+    void checkTotalWithSomeLogs() {
+        given()
+                .log().uri()
+                .log().body()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("total", is(20));
+    }
+
+    @Test
+    void checkTotalGoodPractice() {
+        Integer expectedTotal = 20;
+        Integer actualTotal = given()
+                .log().uri()
+                .log().body()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .extract()
+                .path("total");
+
+        assertEquals(expectedTotal, actualTotal);
+    }
+
+    @Test
+    void checkResponseBadPractice() {
+        String expectedResponse = "{\"total\":20,\"used\":0,\"queued\":0,\"pending\":0,\"browsers\":{\"android\":{\"8.1\":{}},\"chrome\":{\"100.0\":{},\"99.0\":{}},\"chrome-mobile\":{\"86.0\":{}},\"firefox\":{\"97.0\":{},\"98.0\":{}},\"opera\":{\"84.0\":{},\"85.0\":{}}}}\n ";
+
+        Response actualResponse = given()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .extract().response();
+
+        System.out.println(actualResponse);
+
+        String actualResponseString = actualResponse.asString();
+        System.out.println(actualResponseString);
+
+        assertEquals(expectedResponse, actualResponseString);
+    }
+
+    @Test
+    void checkChromeVersion() {
+        given()
+                .log().all()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("browsers.chrome", hasKey("100.0"));
+    }
+
+
+    @Test
+    void checkWDHubStatus() {
+        given()
+                .log().uri()
+                .log().body()
+                .when()
+                .get("https://selenoid.autotests.cloud/wd/hub/status")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(401);
+    }
+
+    @Test
+    void checkWDHubWithAuth() {
+        given()
+                .log().uri()
+                .log().body()
+                .when()
+                .get("https://user1:1234@selenoid.autotests.cloud/wd/hub/status")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("value.ready", is(true));
+    }
+    @Test
+    void checkWDHub() {
+        given()
+                .auth().basic("user1", "1234")
+                .log().all()
+                .when()
+                .get("https://selenoid.autotests.cloud/wd/hub/status")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("value.ready", is(true));
+    }
+
+}
